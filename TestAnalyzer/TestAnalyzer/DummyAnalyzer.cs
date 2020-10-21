308da98d88d4c79266f6ed52789c3aa46360518d
HACK3RMAN <mridudeeppathak@gmail.com>
2020-10-22 01:27:31 +0530

    file puhed

---
+++ TestAnalyzer/TestAnalyzer/DummyAnalyzer.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace TestAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DummyAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "Analyzer1";
+        internal static readonly LocalizableString Title = "Analyzer1 Title";
+        internal static readonly LocalizableString MessageFormat = "Analyzer1 '{0}'";
+        internal const string Category = "Analyzer1 Category";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+
+            // Find just those named type symbols with names containing lowercase letters.
+            if (namedTypeSymbol.Name.Equals("Dummy"))
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
